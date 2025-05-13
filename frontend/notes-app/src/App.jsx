53498be6b0406e20dd78e7e475b3a4b4b5e25b4d
VanNhii <nhivodmx2003@gmail.com>
2025-05-14 01:58:24 +0700

    Fix: deploy frontend - thêm icon cho tag sửa tên tag /

--- frontend/notes-app/src/App.jsx
+++ frontend/notes-app/src/App.jsx
@@ -1,3 +1,23 @@
+// import React from 'react'
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+// import Home from './pages/Home/Home'
+// import Login from './pages/Login/Login'
+// import SignUp from './pages/SignUp/SignUp'
+
+// const App = () => {
+//   return (
+//     <Router>
+//       <Routes>
+//         <Route path="/" element={<Login />} />
+//         <Route path="/dashboard" element={<Home />} />
+//         <Route path="/login" element={<Login />} />
+//         <Route path="/signup" element={<SignUp />} />
+//       </Routes>
+//     </Router>
+//   );
+// };
+
+// export default App;
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home/Home'
@@ -18,3 +38,4 @@ const App = () => {
 };
 
 export default App;
+
