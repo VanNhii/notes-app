07d288997beb4aec2cb97d8fd75f772da149dbde
VanNhii <nhivodmx2003@gmail.com>
2025-05-14 01:08:42 +0700

    Fix: deploy frontend

--- frontend/notes-app/src/utils/constants.js
+++ frontend/notes-app/src/utils/constants.js
@@ -1,7 +1,1 @@
-    //export const BASE_URL = 'http://localhost:8000'
-    // export const BASE_URL = 'https://notes-app-p13k.onrender.com'
-export const BASE_URL = 
-  process.env.REACT_APP_API_BASE_URL ||  // Ưu tiên dùng biến môi trường nếu tồn tại
-  (process.env.NODE_ENV === 'production' 
-    ? 'https://notes-app-p13k.onrender.com'  // Fallback 1: Production URL
-    : 'http://localhost:8000');              // Fallback 2: Localhost
+export const BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:8000'
