0640c74c883b4a353ad132cd35e6a5e75331f224
VanNhii <nhivodmx2003@gmail.com>
2025-05-13 22:56:00 +0700

    Fix front end URL 'http://localhost:8000 to link render

--- frontend/notes-app/src/utils/constants.js
+++ frontend/notes-app/src/utils/constants.js
@@ -1,5 +1,7 @@
     //export const BASE_URL = 'http://localhost:8000'
     // export const BASE_URL = 'https://notes-app-p13k.onrender.com'
-    export const BASE_URL = process.env.NODE_ENV === 'production'
-  ? 'https://notes-app-p13k.onrender.com'
-  : 'http://localhost:8000';
+export const BASE_URL = 
+  process.env.REACT_APP_API_BASE_URL ||  // Ưu tiên dùng biến môi trường nếu tồn tại
+  (process.env.NODE_ENV === 'production' 
+    ? 'https://notes-app-p13k.onrender.com'  // Fallback 1: Production URL
+    : 'http://localhost:8000');              // Fallback 2: Localhost
